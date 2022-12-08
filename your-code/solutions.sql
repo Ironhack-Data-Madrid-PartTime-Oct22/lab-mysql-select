cd851781ae6f36c73335b98a354eb36d7b929322
annassanchez <annassanchez@gmail.com>
2022-12-09 00:02:55 +0100

    cambiando para que esté todo en mayúsculas, no me gusta gritar en sql {{{(>_<)}}}

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -3,15 +3,15 @@
 para mejorar la legibilidad, no he usado el natural join y 
 he cambiado los nombres de los campos para que sean como el ejemplo
 */
-select authors.au_id as author_ID, authors.au_lname as last_name,  
-authors.au_fname as first_name, titles.title, publishers.pub_name as publisher
-from titleauthor
-inner join authors
-on titleauthor.au_id = authors.au_id
-inner join titles
-on titleauthor.title_id = titles.title_id
-inner join publishers
-on titles.pub_id = publishers.pub_id
+SELECT authors.au_id AS author_ID, authors.au_lname AS last_name,  
+authors.au_fname AS first_name, titles.title, publishers.pub_name AS publisher
+FROM titleauthor
+INNER JOIN authors
+ON titleauthor.au_id = authors.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers
+ON titles.pub_id = publishers.pub_id
 ;
 
 /* challenge 02: Who Have Published How Many At Where?*/
@@ -20,18 +20,18 @@ tomo la estructura del challenge anterior.
 se le añade a la consulta el count de los autores y se agrupa por autor y editorial
 */
 
-select authors.au_id as author_ID, authors.au_lname as last_name,  
-authors.au_fname as first_name, publishers.pub_name as publisher, 
+SELECT authors.au_id AS author_ID, authors.au_lname AS last_name,  
+authors.au_fname AS first_name, publishers.pub_name AS publisher, 
 count(titleauthor.au_id) AS title_count
-from titleauthor
-inner join authors
-on titleauthor.au_id = authors.au_id
-inner join titles
-on titleauthor.title_id = titles.title_id
-inner join publishers
-on titles.pub_id = publishers.pub_id
-group by titleauthor.au_id, publishers.pub_id
-order by title_count desc
+FROM titleauthor
+INNER JOIN authors
+ON titleauthor.au_id = authors.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers
+ON titles.pub_id = publishers.pub_id
+GROUP BY titleauthor.au_id, publishers.pub_id
+ORDER BY title_count DESC
 ;
 
 /* challenge 03 - Best Selling Authors*/
@@ -39,39 +39,39 @@ order by title_count desc
 tomo la estructura del challenge anterior.
 se le añade a la consulta el count de los autores y se agrupa por autor y editorial
 */
-select authors.au_id as author_ID, authors.au_lname as last_name,  
-authors.au_fname as first_name, 
+SELECT authors.au_id AS author_ID, authors.au_lname AS last_name,  
+authors.au_fname AS first_name, 
 sum(sales.qty) AS total 
-from authors
-inner join titleauthor
-on authors.au_id = titleauthor.au_id
-inner join titles
-on titles.title_id = titleauthor.title_id
-inner join sales
-on titles.title_id = sales.title_id
-group by authors.au_id
-order by total desc
-limit 3
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+INNER JOIN sales
+ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY total DESC
+LIMIT 3
 ;
 
 /* challenge 04 - Best Selling Authors*/
 /* 
 tomo la estructura del challenge anterior.
-spara que me salgan valores nulos, en vez de inner join tienen que ser left join -- tengo que tener resultados que no crucen para poder tener nulos y que me funcione el case
+para que me salgan valores nulos, en vez de inner join tienen que ser left join -- tengo que tener resultados que no crucen para poder tener nulos y que me funcione el case
 */
-select authors.au_id as author_ID, authors.au_lname as last_name,  
-authors.au_fname as first_name, 
-case
-	when sum(sales.qty) is null then "0"
-	else sum(sales.qty)
-    end as total
-from authors
-left join titleauthor
-on authors.au_id = titleauthor.au_id
-left join titles
-on titles.title_id = titleauthor.title_id
-left join sales
-on titles.title_id = sales.title_id
-group by authors.au_id
-order by max(sales.qty) desc
+SELECT authors.au_id AS author_ID, authors.au_lname AS last_name,  
+authors.au_fname AS first_name, 
+CASE
+	WHEN sum(sales.qty) IS NULL THEN 0
+	ELSE sum(sales.qty)
+    END AS total
+FROM authors
+LEFT JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles
+ON titles.title_id = titleauthor.title_id
+LEFT JOIN sales
+ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY max(sales.qty) DESC
 ;
