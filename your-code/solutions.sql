cfc1678e1e2a4c1186c13ec5ff7a192e600bb411
ppastorcarrillo <116591098+ppastorcarrillo@users.noreply.github.com>
2022-12-08 12:42:57 +0100

    solucionado, terminando de comprender cuando se usa cada join

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -5,12 +5,16 @@ select * from titleauthor;
 select * from titles;
 select * from publishers;
 
+##CHALLANGE 1## 
+select au_id as "AUTHOR ID", au_lname AS "LAST NAME", au_fname AS "FIRST NAME", title AS "TITLE", pub_name AS "PUBLISHER"
+from authors
+natural join titleauthor 
+natural join titles
+inner join publishers on titles.pub_id = publishers.pub_id;
 
-select au_id , au_lname, au_fname, title, pub_name
-from titles
-natural join titleauthor
-natural join authors
-natural join publishers;
+
+
+
 
 
 
